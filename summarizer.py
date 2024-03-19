44199f3eb71636a020aefcd48e0850730040048b
Abdelkarim Mouachiq <abmou87@ulaval.ca>
2024-03-19 09:26:37 +0000

    add sentence tokenizer

--- summarizer.py
+++ summarizer.py
@@ -4,3 +4,8 @@ import re
 def load_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as f:
         return f.read()
+
+
+def sentence_tokenize(text):
+    sentences = re.split(r'(?<=[.!?])\s+', text.strip())
+    return [s.strip() for s in sentences if len(s.strip()) > 10]
